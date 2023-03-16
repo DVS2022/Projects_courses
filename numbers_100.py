2a8ae23dd7b377e85de6adfefd3fda3750fafa93
DVS_2022 <sad105@mail.ru>
2023-03-16 11:50:44 +0700

    "Add a file numbers_100.py. Inside 1 function: add_to_list. The application adds numbers from 0 to 100 to the list."

---
+++ numbers_100.py
@@ -0,0 +1,9 @@
+def add_to_list():
+    """ Функция добавляет в список numbers числа от 0 до 100. """
+    numbers = []
+    for i in range(101):
+        numbers.append(i)
+    print(numbers)
+
+
+add_to_list()
