27ab1ad533baeda8462f6043b1f23a49ec2874f5
DVS_2022 <sad105@mail.ru>
2023-03-21 21:46:06 +0700

    "Add a file: lst_comp_cubes_squares.py. Inside 1 function: cubes_and_squares(). The application creates lists of squares and cubes of numbers in the given range."

---
+++ list_comprehensions/lst_comp_cubes_squares.py
@@ -0,0 +1,12 @@
+def cubes_and_squares():
+    """ Функция создает списки квадратов и кубов чисел в
+    заданном диапазоне. """
+    start = int(input('Введите начало списка: '))
+    stop = int(input('Введите конец списка: '))
+    print(f'Список квадратов чисел от {start} до {stop}',
+          [x**3 for x in range(start, stop+1)])
+    print(f'Список кубов чисел от {start} до {stop}',
+          [x**2 for x in range(start, stop+1)])
+
+
+cubes_and_squares()
