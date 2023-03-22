bd33fa84c90525c004b7dfb7d77f7b87d2ce318f
DVS_2022 <sad105@mail.ru>
2023-03-22 19:10:40 +0700

    "Add a file: lst_comp_list_generate_and_compare.py. Inside 1 function: list_generate_and_compare(). The application generates two lists with real numbers, combines them according to the principle of large values."

---
+++ list_comprehensions/lst_comp_generate_and_compare.py
@@ -0,0 +1,16 @@
+import random
+
+
+def list_generate_and_compare():
+    """ Generates two lists with real numbers, combines them according
+    to the principle of large values. """
+    lst_1 = [round(random.uniform(0, 10.00), 2) for _ in range(20)]
+    lst_2 = [round(random.uniform(0, 10.00), 2) for _ in range(20)]
+    lst_compare = [lst_1[i] if lst_1[i] > lst_2[i] else
+                   lst_2[i] for i in range(len(lst_1))]
+    print(lst_1)
+    print(lst_2)
+    print(lst_compare)
+
+
+list_generate_and_compare()
