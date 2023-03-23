0179e08ddc61954461198c869dfacede8a661592
DVS_2022 <sad105@mail.ru>
2023-03-23 12:14:08 +0700

    "Add a file: lst_comp_zip_list.py. Inside 1 function: zip_list(). The application rearrange all non-zero elements to the beginning and remove all zeros from the list."

---
+++ list_comprehensions/lst_comp_zip_list.py
@@ -0,0 +1,17 @@
+def zip_list():
+    """ Rearrange all non-zero elements to the beginning and
+        remove all zeros from the list.
+    """
+    lst = [0, 1, 2, 0, 3, 4, 0, 5, 6, 7, 0, 0, 8, 9]
+    print(lst)
+    count = 0
+    for i in range(len(lst)):
+        if lst[i] != 0:
+            lst[count], lst[i] = lst[i], lst[count]
+            count += 1
+    print(lst)
+    lst = lst[:count]
+    print(lst)
+
+
+zip_list()
