3757604aabcf250e01afce94d5442db297ee42ec
DVS_2022 <sad105@mail.ru>
2023-03-24 16:38:50 +0700

    "Add a file: str_format_1.py. Inside 1 function: name_and_number(). The application requests the name and number of the table and displays the appropriate message."

--- list_comprehensions/str_format_1.py
+++ list_comprehensions/str_format_1.py
@@ -1,6 +1,7 @@
 def name_and_number():
     """ Requests the name and number of the table and
-    displays the appropriate message."""
+    displays the appropriate message.
+    """
     name = str(input('Введите имя: '))
     number = str(input('Введите номер столика: '))
     string = 'Добрый день, {0}! Ваш столик № {1}'.format(
