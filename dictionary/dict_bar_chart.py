d6a4f6b1b9557af478ada99ad1b501cf858887d3
DVS_2022 <sad105@mail.ru>
2023-04-02 15:24:18 +0700

    "Add a file: dict_bar_chart.py. Inside 1 function: bar_chart(). The application takes user input for a string of text and creates a bar chart displaying the frequency of each symbol (character) in the text and shows max frequency."

---
+++ dictionary/dict_bar_chart.py
@@ -0,0 +1,17 @@
+def bar_chart():
+    """ Takes user input for a string of text and creates a bar
+    chart displaying the frequency of each symbol (character) in
+    the text and shows max frequency. """
+    text = input('Введите текст: ').lower()
+    symbol_dict = dict()
+    for symbol in text:
+        if symbol in symbol_dict:
+            symbol_dict[symbol] += 1
+        else:
+            symbol_dict[symbol] = 1
+    for i_sym in sorted(symbol_dict.keys()):
+        print(i_sym, '-', symbol_dict[i_sym])
+    print('Наибольшая частота:', max(symbol_dict.values()))
+
+
+bar_chart()
