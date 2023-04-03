2634f214eba6f5d046a21201491bbaea3abfeb3d
DVS_2022 <sad105@mail.ru>
2023-04-03 15:32:57 +0700

    "Add a file: dict_multi_get.py. Inside 1 function: multi_get(). The application extracts data from the given structure. removed print(family_member)"

---
+++ dictionary/dict_multi.py
@@ -0,0 +1,31 @@
+def multi_get():
+    """ Extracts data from the given structure. """
+    family_member = {
+        'name': 'Наталия',
+        'surname': 'Дёмочкина',
+        'hobbies': ['чтение, плавание'],
+        'age': 34,
+        'children': [
+            {
+                'name': 'Александр',
+                'age': 7
+            },
+            {
+                'name': 'Максим',
+                'age': 5
+            }
+        ]
+    }
+    children = family_member.get('children', [])
+    search_name = input('Введите имя ребенка: ')
+    flag = False
+    print(family_member)
+    for i in children:
+        if i['name'] == search_name:
+            print('Есть такой ребенок!')
+            flag = True
+    if not flag:
+        print('Нет такого ребенка!')
+
+
+multi_get()
